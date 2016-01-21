2fed43d011eda0a0714dc103d63e937c4ac0db01
SAKAI RYUICHI <JP20105@w022341308113m.local>
2016-01-21 14:33:02 +0900

    Exercise 4.4

---
+++ chapter04/Exercise04.scala
@@ -0,0 +1,16 @@
+object Main {
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    a flatMap (x => (b map (y => f(x, y))))
+
+  def sequence[A](oas: List[Option[A]]): Option[List[A]] = oas match {
+    case Nil => Some(Nil)
+    case (x::xs) => map2(x, sequence(xs))((a, as) => a::as)
+  }
+    
+  def main(args: Array[String]): Unit = {
+    val loOk = List(Option(1), Option(2), Option(3))
+    val loNg = List(Option(1), None, Option(3))
+    println(sequence(loOk))
+    println(sequence(loNg))
+  }
+}
